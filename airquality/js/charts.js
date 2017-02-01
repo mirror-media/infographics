27a62418a6fb7469dcb940700b8c30b3762cfa2a
Chaowen Chang <imkojima@me.com>
2017-02-02 02:47:06 +0800

    charts animation

--- airquality/js/charts.js
+++ airquality/js/charts.js
@@ -1,4 +1,27 @@
+(function (H) {
+    function deferRender (proceed) {
+        var series = this, 
+            $renderTo = $(this.chart.container.parentNode);
+
+        // It is appeared, render it
+        if ($renderTo.is(':appeared') || !series.options.animation) {
+            proceed.call(series);
+            
+        // It is not appeared, halt renering until appear
+        } else  {
+            $renderTo.appear(); // Initialize appear plugin
+            $renderTo.on('appear', function () {
+                proceed.call(series);
+            });
+        }
+    };
+    
+    H.wrap(H.Series.prototype, 'render', deferRender);
+    
+}(Highcharts));
+
 $(function () {
+
     Highcharts.setOptions({
         lang: {
             numericSymbolMagnitude: 10,
