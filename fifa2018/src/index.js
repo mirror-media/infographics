8d92c16255c1284c529c0b52a1105dcce642ddcc
vinthen <nehtniv@gmail.com>
2018-05-22 01:04:50 +0800

    fifa2018 add quiz

--- fifa2018/src/index.js
+++ fifa2018/src/index.js
@@ -1,35 +1,45 @@
 /* ---------- plugins ----------*/
 import superagent from "superagent";
+import _ from "lodash";
 import Swiper from "../plugins/swiper.js";
 
 /* ---------- import data ----------*/
-import {quizinit} from "./quiz.js";
-
+import { initSwiper, initQuiz, quizOption } from "./quiz.js";
 
 if (document.querySelector(".quizwpr") != null) {
-  /* ---------- Swiper ----------*/
-  let quizSwiper = new Swiper(".swiper-container", {
+  var blackboard = null;
+  var quizSwiper = null;
 
-    allowTouchMove: false,
-    pagination: {
-      el: ".swiper-pagination",
-      type: 'fraction'
-    }
-
-  });
-
-  quizSwiper.on('reachEnd',() => {
-    console.log('swiper end!');
-  });
-
-  /* ---------- quiz ----------*/
+  /* ----- team score array -----*/
   superagent
     .get("/data/team.json")
     .then(function(res) {
+      blackboard = JSON.parse(res.text);
 
-      const team = JSON.parse(res.text);
-      quizinit(quizSwiper,team);
+      console.log(blackboard);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 
+  /* ----- init swiper and quiz content -----*/
+  superagent
+    .get("/data/quiz.json")
+    .then(function(res) {
+      const quizData = JSON.parse(res.text);
+      // console.log(JSON.parse(res.text));
+      // initSwiper(Swiper);
+      initQuiz(quizData);
+
+      initSwiper(Swiper);
+      quizOption();
+      
+      // setTimeout(() => {
+        
+      //   quizOption();
+      // },200);
+      
+      
     })
     .catch(function(err) {
       console.log(err);
--- fifa2018/src/quiz.js
+++ fifa2018/src/quiz.js
@@ -1,12 +1,74 @@
-/* ---------- quiz ----------*/
-export function quizinit(quizSwiper,team) {
-//   console.log(typeof(team[1].score));
-    document.querySelectorAll('.quiz--a').forEach((element) => {
-        element.addEventListener('click',() => {
-            quizSwiper.slideNext();
-        }, false);
-    })
+/* ---------- Swiper 初始化 ----------*/
+export function initSwiper(Swiper){
+    window.quizSwiper = new Swiper(".swiper-container", {
+  
+      allowTouchMove: false,
+      pagination: {
+        el: ".swiper-pagination",
+        type: 'fraction'
+      }
+  
+    });
+  
+    quizSwiper.on('reachEnd',() => {
+      console.log('swiper end!');
+    });
+  }
+
+/* ---------- 建立題目 ----------*/
+export function initQuiz(quizData){
+
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
+
+    quizData.forEach((element) => {          
+
+        /* ----- create swiper slide ----- */
+        const entry = 
+        `<div class="quiz">
+                <h2 class="quiz--question">${element.question}</h2>                
+        </div>`.trim();
+
+        let slide = document.createElement('div');
+        slide.classList.add('swiper-slide');
+        slide.innerHTML = entry;
+
+        swiperWrapper.appendChild(slide);
+
+        /* ----- create slide options ----- */
+        element.option.forEach((element) => {
+
+            let option = document.createElement('div');
+            option.classList.add('quiz--option');
+            option.innerHTML = element.content;
+            option.setAttribute('data-team', `${element.team}`);
+
+            slide.querySelector('.quiz').appendChild(option);           
+
+        });
+
+    });
+}
+
+/* ---------- quiz 行為 ----------*/
+export function quizOption() {
+
+  //   console.log(typeof(team[1].score));
+  document.querySelectorAll(".quiz--option").forEach(element => {
+    element.addEventListener(
+      "click",
+      () => {
+        window.quizSwiper.slideNext();
+        console.log(element.dataset.team);
+
+      },
+      false
+    );
+  });
+
+}
 
 
 
+export function resetQuiz() {
+  console.log("再玩一次");
 }
