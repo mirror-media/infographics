76b96c5dc93819fcd0a0e66626fe8e45195895e7
vinthen <nehtniv@gmail.com>
2018-07-04 17:59:03 +0800

    add weichuan soysauce project

---
+++ weichuan/src/index.js
@@ -0,0 +1,158 @@
+// jQuery
+window.$ = require('jquery');
+window.jQuery = require('jquery');
+
+// localScroll (require scrollto)
+const scrollto = require('jquery.scrollto');
+const localScroll = require('jquery.localscroll');
+
+// Home: parallax hero image, slick slider
+const parallax = require('jquery-parallax.js');
+const slick = require('slick-carousel');
+
+
+$(document).ready(function(){
+
+// Home: hero image parallax
+$(".hero--img").parallax({
+    imageSrc: "images/hero.jpg",
+    position: "left top",
+    speed: 0.2
+});    
+
+// Home: slick slider    
+$(".slider--container").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    prevArrow: '<div class="slick-prev">Previous</div>',
+    nextArrow: '<div class="slick-next">Next</div>',
+    responsive: [
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '35px'
+            }
+        },
+        {
+            breakpoint: 350,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+    // autoplay: true
+});
+
+const $menuContainer = $("#menuContainer");
+
+// open aside menu
+function openMenu(){
+    $menuContainer.addClass("active");
+    $("html").addClass("lock");
+}
+
+// close aside menu
+function closeMenu(){
+    $menuContainer.removeClass("active");
+    $("html").removeClass("lock");
+}
+
+// aside menu
+$("#menuTrigger").click(function(){
+    openMenu();
+});
+
+$menuContainer.children(".overlay").click(function(){    
+    closeMenu();
+});
+
+$(".menuClose").click(function(){    
+    closeMenu();
+});
+
+$(".menu--item a").click(function(){
+    closeMenu();
+});
+
+if($("body").hasClass("home")){
+    //local scroll
+    $.localScroll({
+        duration: 350,
+        hash: false
+    });
+}
+
+// back top button
+$("#backTop").click(function(){
+	$("body,html").animate({
+		scrollTop: 0
+	}, 400);
+	return false;
+});
+
+// show/hide back top button
+function showBacktop(fixBtn){
+    let scroll_top = $(window).scrollTop();
+
+    //scroll top btn
+    if(scroll_top > 100){
+        fixBtn.addClass("active");
+    } else {
+        fixBtn.removeClass("active");
+    }
+    
+}
+
+// window scroll
+window.addEventListener("scroll",() => {
+
+    showBacktop($("#fixBtn"));
+
+}, false);
+
+
+
+
+
+
+}); // document ready
+
+
+
+
+// if($("body").hasClass("home")){
+
+//     //local scroll
+//     $.localScroll({
+//         duration: 350,
+//         hash: false
+//     });
+
+// }
+
+// let $menuContainer = $("#menuContainer");
+
+// function openMenu(){
+//     $menuContainer.addClass("active");
+//     $("html").addClass("lock");
+// }
+
+// function closeMenu(){
+//     $menuContainer.removeClass("active");
+//     $("html").removeClass("lock");
+// }
+
+// openMenu();
---
+++ weichuan/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src",
+  output: {    
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+
+  mode: "production", // "production" | "development" | "none"
+  watch: true,
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
