6ac9467d94e57814da7eaa36f43dd062bc00c42a
KJ WEN <kjwen310@gmail.com>
2021-10-12 17:19:21 +0800

    refactor: change flourish from iframe to embedded code (#133)

--- nft/components/ReportQuiz.vue
+++ nft/components/ReportQuiz.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="quiz">
     <!-- eslint-disable vue/no-v-html -->
-    <div class="quiz__iframe" v-html="iframeSrc" />
+    <!-- <div class="quiz__iframe" v-html="iframeSrc" /> -->
+    <div
+      class="flourish-embed flourish-quiz"
+      data-src="visualisation/7190892"
+    />
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      iframeSrc: `
-        <iframe
-          src='https://flo.uri.sh/visualisation/7190892/embed'
-          title='Interactive or visual content'
-          class='flourish-embed-iframe'
-          frameborder='0'
-          scrolling='no'
-          style='width:100%;height:100%;overflow:hidden;'
-          sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
-        ></iframe>
-      `,
-    }
+  // data() {
+  //   return {
+  //     iframeSrc: `
+  //       <iframe
+  //         src='https://flo.uri.sh/visualisation/7190892/embed'
+  //         title='Interactive or visual content'
+  //         class='flourish-embed-iframe'
+  //         frameborder='0'
+  //         scrolling='no'
+  //         style='width:100%;height:100%;'
+  //         sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+  //       ></iframe>
+  //     `,
+  //   }
+  // },
+  mounted() {
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.crossOrigin = true
+    script.src = 'https://public.flourish.studio/resources/embed.js'
+    document.body.appendChild(script)
   },
 }
 </script>
@@ -28,27 +39,27 @@ export default {
 <style lang="scss" scoped>
 .quiz {
   width: 100%;
-  height: 3660px;
-  min-height: 3660px;
-  @include media-breakpoint-up(sm) {
-    height: 3900px;
-    min-height: 3900px;
-  }
-  @include media-breakpoint-up(md) {
-    height: 4200px;
-    min-height: 4200px;
-  }
-  @include media-breakpoint-up(xl) {
-    height: 5660px;
-    min-height: 5660px;
-  }
-  @include media-breakpoint-up(xxl) {
-    height: 5700px;
-    min-height: 5700px;
-  }
-  &__iframe {
-    width: 100%;
-    height: 100%;
-  }
+  // height: 3660px;
+  // min-height: 3660px;
+  // @include media-breakpoint-up(sm) {
+  //   height: 3900px;
+  //   min-height: 3900px;
+  // }
+  // @include media-breakpoint-up(md) {
+  //   height: 4200px;
+  //   min-height: 4200px;
+  // }
+  // @include media-breakpoint-up(xl) {
+  //   height: 5660px;
+  //   min-height: 5660px;
+  // }
+  // @include media-breakpoint-up(xxl) {
+  //   height: 5700px;
+  //   min-height: 5700px;
+  // }
+  // &__iframe {
+  //   width: 100%;
+  //   height: 100%;
+  // }
 }
 </style>
