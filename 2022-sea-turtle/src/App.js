bfa37229301f336b1271a6e44c57f1b8a8f8800c
DianYangFu <dianyangfu@gmail.com>
2022-06-02 15:08:53 +0800

    feat(2022-sea-turtle): add views `catalog` and corresponding function for changing view

--- 2022-sea-turtle/src/App.js
+++ 2022-sea-turtle/src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Intro from './views/intro';
+import Catalog from './views/catalog';
+
 function App() {
+  const [shouldShowCatalog, setShouldShowCatalog] = useState(false);
+
+  const changeView = (value) => {
+    if (value) {
+      setShouldShowCatalog(value);
+    }
+  };
   return (
     <div className="App">
-      <Intro />
+      {!shouldShowCatalog && <Intro changeView={changeView} />}
+      {shouldShowCatalog && <Catalog />}
     </div>
   );
 }
