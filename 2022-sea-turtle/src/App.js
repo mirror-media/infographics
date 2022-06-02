e4b4c810069dcbc66c6e302c61f869966794bee2
DianYangFu <dianyangfu@gmail.com>
2022-06-02 23:19:37 +0800

    feat(2022-sea-turtle): add component `comic-header`

--- 2022-sea-turtle/src/App.js
+++ 2022-sea-turtle/src/App.js
@@ -1,20 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Intro from './views/intro';
 import Catalog from './views/catalog';
+import Comic from './views/comic';
+// import CatalogHeader from './components/catalog-header';
+import ComicHeader from './components/comic-header';
+const BackgroundWrapper = styled.div`
+  background: #f8f3e8;
+`;
 
 function App() {
   const [shouldShowCatalog, setShouldShowCatalog] = useState(false);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [shouldShowCatalog]);
 
   const changeView = (value) => {
     if (value) {
-      setShouldShowCatalog(value);
+      setTimeout(() => setShouldShowCatalog(value), 500);
     }
   };
+
   return (
     <div className="App">
       {!shouldShowCatalog && <Intro changeView={changeView} />}
-      {shouldShowCatalog && <Catalog />}
+
+      {shouldShowCatalog && (
+        <BackgroundWrapper>
+          {/* <CatalogHeader /> */}
+          <ComicHeader />
+          <Catalog />
+          <Comic />
+        </BackgroundWrapper>
+      )}
     </div>
   );
 }
---
+++ 2022-sea-turtle/src/views/comic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ComicWrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export default function Comic() {
+  return <ComicWrapper></ComicWrapper>;
+}
