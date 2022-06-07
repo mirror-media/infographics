bef67ccb5ecc7634ad0dd1122c174eb42176f5c6
DianYangFu <dianyangfu@gmail.com>
2022-06-07 16:52:39 +0800

    feat(2022-sea-turtle): add link to certain comic by adding hash fragment

--- 2022-sea-turtle/src/utils/scroll-into-comic.js
+++ 2022-sea-turtle/src/utils/scroll-into-comic.js
@@ -1,6 +1,6 @@
 const scrollIntoComic = (id) => {
   if (id === 'nightmare' || id === 'holic') {
-    const comicId = document.querySelector(`#${id}`);
+    const comicId = document.querySelector(`.${id}`);
     comicId.scrollIntoView({ behavior: 'smooth' });
   } else {
     window.open('https://www.google.com/');
